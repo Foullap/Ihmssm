85141c12dc5fd334e353cc890d58f2298474d9f0
Foullap <154831356+Foullap@users.noreply.github.com>
2023-12-28 16:38:10 +0300

    Update all 1.py

--- Tasks/all 1.py
+++ Tasks/all 1.py
@@ -4,9 +4,7 @@ def print_number_k(k, n):
 
 k = int(input("Введите целое число K: "))
 n = int(input("Введите целое число N (N > 0): "))
-if n > 0:
+if n > 0 and k > 0:
     print_number_k(k, n)
 else:
-    print("Число N должно быть больше 0")
-
-
+    print("Числа N и K должны быть больше 0")
